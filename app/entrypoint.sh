fadbbb09147e4159ae517ac567dc3e31c1f95b2b
swelanauguste@gmail.com <swelanauguste@gmail.com>
2021-07-23 17:27:36 -0400

    docker

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-# if [ "$DATABASE" = "postgres" ]
-# then
-#     echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-#     while ! nc -z $SQL_HOST $SQL_PORT; do
-#       sleep 0.1
-#     done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
 
-#     echo "PostgreSQL started"
+    echo "PostgreSQL started"
 # fi
 
 
 # python manage.py flush --noinput
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic --noinput
 python manage.py createsuperuser --email swelanauguste@gmail.com --username superuser --noinput
 python manage.py add_categories categories.txt
 python manage.py add_tags tags.txt
